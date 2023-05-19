610c17dec5227a8e4cd5c567a8e9c0b155fc3dbd
Murat ÇINAR <muratcinar.dev@gmail.com>
2023-05-19 11:56:42 +0300

    first commit

---
+++ EasyCashIdentityProject.DataAccesLayer/Concrete/Context.cs
@@ -0,0 +1,16 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concreate;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyCashIdentityProject.DataAccesLayer.Concrete
+{
+    public class Context : IdentityDbContext<AppUser, AppRole, int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MURAT;initial catalog=EasyCashDb;integrated Security=true");
+        }
+        public DbSet<CustomerAccount> CustomerAccounts  { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountsProceses { get; set; }
+    }
+}
