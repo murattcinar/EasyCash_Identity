96f54680062b546dd7448b39c3c105dac2f4dae1
Murat ÇINAR <muratcinar.dev@gmail.com>
2023-05-20 14:17:21 +0300

    update-5

---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/ConfirmMailController.cs
@@ -0,0 +1,38 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concreate;
+using EasyCashIdentityProject.PresentationLayer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+	public class ConfirmMailController : Controller
+	{
+		private readonly UserManager<AppUser> _userManager;
+
+        public ConfirmMailController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+		public IActionResult Index()
+		{
+			var value = TempData["Mail"];
+			ViewBag.v=value;
+			//confirmMailViewModel.Mail = value.ToString;
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Index(ConfirmMailViewModel confirmMailViewModel)
+		{
+			var user = await _userManager.FindByEmailAsync(confirmMailViewModel.Mail);
+			if(user.ConfirmCode==confirmMailViewModel.ConfirmCode)
+			{
+				return RedirectToAction("Index", "MyProfile");
+				//email confirmed kısmı update edilip trua'ya dönecek
+			}
+            return View();
+		}
+	}
+}
